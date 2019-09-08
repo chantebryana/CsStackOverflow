4d52d03226869a15e9e73739366c39dccbf7f40a
chantebryana <chantebryana@gmail.com>
2019-09-08 13:38:54 -0500

    Very rough and dirty beginnings.

--- CsStackOverflow/CsStackOverflow/Program.cs
+++ CsStackOverflow/CsStackOverflow/Program.cs
@@ -2,11 +2,32 @@
 
 namespace CsStackOverflow
 {
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private DateTime _created;
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public void Howdy()
+        {
+            Console.WriteLine("Howdy from the class.");
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var post = new Post();
+            post.Howdy();
+            post.Title = "Title 01";
+            post.Description = "An insightful portrait of a developer as a young woman.";
+            post.Created = DateTime.Now;
+            Console.WriteLine("{0}: {1} - Created {2}", post.Title, post.Description, post.Created);
+
         }
     }
 }
