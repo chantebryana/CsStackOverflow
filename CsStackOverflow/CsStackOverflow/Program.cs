b5ea1f6c25ff3fa1585c5439fb8a601a59a5aec3
chantebryana <chantebryana@gmail.com>
2019-09-16 16:07:31 -0500

    finished stack overflow class exercise -- pretty easy

--- CsStackOverflow/CsStackOverflow/Program.cs
+++ CsStackOverflow/CsStackOverflow/Program.cs
@@ -7,13 +7,36 @@ namespace CsStackOverflow
         private string _title;
         private string _description;
         private DateTime _created;
+        private int _votes;
 
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public DateTime Created { get; set; }
-        public void Howdy()
+        public Post()
         {
-            Console.WriteLine("Howdy from the class.");
+        }
+
+        public void NewPost (string titulo, string body)
+        {
+            _title = titulo;
+            _description = body;
+            _created = DateTime.Now;
+        }
+        public void printPost()
+        {
+            Console.WriteLine("\n{0}:\n{1}\n- Created: {2}", _title, _description, _created);
+        }
+        public void printVote()
+        {
+            Console.WriteLine("Current Vote Value: {0}\n", _votes);
+        }
+        public void Vote(Boolean upVote)
+        {
+            if (upVote == true)
+            {
+                _votes++;
+            }
+            else if (upVote == false)
+            {
+                _votes--;
+            }
         }
     }
 
@@ -22,12 +45,40 @@ namespace CsStackOverflow
         static void Main()
         {
             var post = new Post();
-            post.Howdy();
-            post.Title = "Title 01";
-            post.Description = "An insightful portrait of a developer as a young woman.";
-            post.Created = DateTime.Now;
-            Console.WriteLine("{0}: {1} - Created {2}", post.Title, post.Description, post.Created);
-
+            Console.WriteLine("Enter Post Title: ");
+            var a = Console.ReadLine();
+            Console.WriteLine("Enter Post Description: ");
+            var b = Console.ReadLine();
+            post.NewPost(a, b);
+            post.printPost();
+            Console.WriteLine("\n");
+            while (true) {
+                Console.WriteLine("Type 'upvote', 'downvote', 'print', or 'close':");
+                var userInput = Console.ReadLine();
+                userInput = userInput.ToLower();
+                if (userInput == "upvote")
+                {
+                    post.Vote(true);
+                }
+                else if (userInput == "downvote")
+                {
+                    post.Vote(false);
+                }
+                else if (userInput == "close")
+                {
+                    Console.WriteLine("Closing...\n");
+                    break;
+                }
+                else if (userInput == "print")
+                {
+                    post.printPost();
+                    post.printVote();
+                }
+                else
+                {
+                    Console.WriteLine("'{0}' is an invalid selection. Try again.\n", userInput);
+                }
+            }
         }
     }
 }
